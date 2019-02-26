27d6454c433e3f3c500d8dc69512ddec4f5df365
KentaShibuta <q237038k@mail.kyutech.jp>
2019-02-27 01:14:31 +0900

    slimming line of mesh

--- visualization_culculation.py
+++ visualization_culculation.py
@@ -7,11 +7,11 @@ df.columns = ['x', 'y']
 print(df)
 print(df.at[(0, "y")])
 print(df.at[(2, "y")])
-plt.scatter(df['x'],df['y'])
+#plt.scatter(df['x'],df['y'])#ここを入れると節点が強調して表示される．
 
 for i in range(440):
     if df.at[(i, "x")] < df.at[(i+1, "x")]:
-        plt.plot([df.at[(i, "x")], df.at[(i+1, "x")]], [df.at[(i, "y")], df.at[(i+1, "y")]],'k-', lw=2)
+        plt.plot([df.at[(i, "x")], df.at[(i+1, "x")]], [df.at[(i, "y")], df.at[(i+1, "y")]],'k-', lw=0.7)
 for i in range(420):#nnode-n_ynode
-    plt.plot([df.at[(i, "x")], df.at[(i+21, "x")]], [df.at[(i, "y")], df.at[(i+21, "y")]],'k-', lw=2)
+    plt.plot([df.at[(i, "x")], df.at[(i+21, "x")]], [df.at[(i, "y")], df.at[(i+21, "y")]],'k-', lw=0.7)
 plt.show()
