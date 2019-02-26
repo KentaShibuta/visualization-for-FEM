6b4565dd29bc590812a26af23398c1b533d40614
KentaShibuta <q237038k@mail.kyutech.jp>
2019-02-26 20:36:19 +0900

    visualization of node

---
+++ visualization_culculation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+
+df = pd.read_csv('filename.csv',header=None)
+df.columns = ['X', 'Y']
+print(df)#勝手に行番号と列番号を指定してくれる．
+#print(df['X'])
+plt.scatter(df['X'],df['Y'])
+#plt.plot(df['X'],df['Y'])
+plt.show()
